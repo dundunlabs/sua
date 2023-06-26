b0627705b40242292a6275390f628f93bf1cfc92
Dung Tran <dung28.td@gmail.com>
2023-06-26 11:35:44 +0700

    simple insert statement

--- stmt.go
+++ stmt.go
@@ -1,11 +1,17 @@
 package sua
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
 
 type stmtOp string
 
 const (
 	opSelect stmtOp = "SELECT"
+	opInsert stmtOp = "INSERT"
 	opDelete stmtOp = "DELETE"
 )
 
@@ -15,9 +21,10 @@ func newStmt(db *DB) *stmt {
 
 type stmt struct {
 	*DB
-	op    stmtOp
-	table table
-	cols  columns
+	op     stmtOp
+	table  table
+	cols   columns
+	models []map[string]any
 }
 
 func (s *stmt) Table(name string, alias string) *stmt {
@@ -32,27 +39,92 @@ func (s *stmt) Select(cols ...string) *stmt {
 	return ns
 }
 
+func (s *stmt) Insert(models ...map[string]any) *stmt {
+	ns := s.clone()
+	ns.models = append(ns.models, models...)
+	return ns
+}
+
 func (s *stmt) SQL() string {
-	ns := s.table.aliasOrName()
 	switch s.op {
 	case opSelect:
-		return fmt.Sprintf(
-			"%s %s FROM %q AS %q",
-			s.op,
-			s.cols.string(ns),
-			s.table.name,
-			ns,
-		)
+		return s.selectSql()
+	case opInsert:
+		return s.insertSql()
 	case opDelete:
-		return fmt.Sprintf(
-			"%s FROM %q AS %q",
-			s.op,
-			s.table.name,
-			ns,
-		)
+		return s.deleteSql()
 	default:
 		return ""
 	}
+}
+
+func (s *stmt) selectSql() string {
+	ns := s.table.aliasOrName()
+
+	return fmt.Sprintf(
+		"%s %s FROM %q AS %q",
+		s.op,
+		s.cols.string(ns),
+		s.table.name,
+		ns,
+	)
+}
+
+func (s *stmt) insertSql() string {
+	fields := s.fields()
+	cols := []string{}
+	for _, f := range fields {
+		cols = append(cols, fmt.Sprintf("%q", f))
+	}
+
+	values := []string{}
+	for _, m := range s.models {
+		value := []string{}
+		for _, f := range fields {
+			if v, ok := m[f]; ok {
+				switch v := v.(type) {
+				case string:
+					value = append(value, fmt.Sprintf("'%s'", v))
+				default:
+					value = append(value, fmt.Sprintf("%v", v))
+				}
+			} else {
+				value = append(value, "DEFAULT")
+			}
+		}
+		values = append(values, fmt.Sprintf("(%s)", strings.Join(value, ", ")))
+	}
+
+	return fmt.Sprintf(
+		"%s INTO %q (%s) VALUES %s",
+		opInsert,
+		s.table.name,
+		strings.Join(cols, ", "),
+		strings.Join(values, ", "),
+	)
+}
+
+func (s *stmt) deleteSql() string {
+	ns := s.table.aliasOrName()
+
+	return fmt.Sprintf(
+		"%s FROM %q AS %q",
+		s.op,
+		s.table.name,
+		ns,
+	)
+}
+
+func (s *stmt) fields() []string {
+	fields := []string{}
+	for _, m := range s.models {
+		for k, _ := range m {
+			if !slices.Contains(fields, k) {
+				fields = append(fields, k)
+			}
+		}
+	}
+	return fields
 }
 
 func (s *stmt) clone() *stmt {
--- stmt_test.go
+++ stmt_test.go
@@ -8,6 +8,7 @@ import (
 
 var (
 	mockSelectStmt = &stmt{op: opSelect}
+	mockInsertStmt = &stmt{op: opInsert}
 	mockDeleteStmt = &stmt{op: opDelete}
 )
 
@@ -19,6 +20,23 @@ func TestSelect(t *testing.T) {
 	test.Equal(t, s.SQL(), `SELECT "u".* FROM "users" AS "u"`)
 	test.Equal(t, s1.SQL(), `SELECT "u"."id", "u"."name" FROM "users" AS "u"`)
 	test.Equal(t, s2.SQL(), `SELECT "u"."id", "u"."name", "u"."gender" FROM "users" AS "u"`)
+}
+
+func TestInsert(t *testing.T) {
+	mockInsertUser := mockInsertStmt.Table("users", "u")
+
+	t.Run("Single", func(t *testing.T) {
+		s := mockInsertUser.Insert(map[string]any{"name": "foo", "gender": "male"})
+		test.Equal(t, s.SQL(), `INSERT INTO "users" ("name", "gender") VALUES ('foo', 'male')`)
+	})
+
+	t.Run("Multiple", func(t *testing.T) {
+		s := mockInsertUser.Insert(
+			map[string]any{"name": "foo"},
+			map[string]any{"gender": "male"},
+		)
+		test.Equal(t, s.SQL(), `INSERT INTO "users" ("name", "gender") VALUES ('foo', DEFAULT), (DEFAULT, 'male')`)
+	})
 }
 
 func TestDelete(t *testing.T) {
