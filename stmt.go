eae267836b43b4ce6426b42add54d32cf4ae055f
Dung Tran <dung28.td@gmail.com>
2023-06-26 10:04:25 +0700

    simple delete statement

--- stmt.go
+++ stmt.go
@@ -1,15 +1,23 @@
 package sua
 
+import "fmt"
+
+type stmtOp string
+
+const (
+	opSelect stmtOp = "SELECT"
+	opDelete stmtOp = "DELETE"
+)
+
 func newStmt(db *DB) *stmt {
-	s := &stmt{DB: db}
-	s.selectStmt = newSelectStmt(s)
-	return s
+	return &stmt{DB: db}
 }
 
 type stmt struct {
 	*DB
+	op    stmtOp
 	table table
-	*selectStmt
+	cols  columns
 }
 
 func (s *stmt) Table(name string, alias string) *stmt {
@@ -18,18 +26,36 @@ func (s *stmt) Table(name string, alias string) *stmt {
 	return ns
 }
 
+func (s *stmt) Select(cols ...string) *stmt {
+	ns := s.clone()
+	ns.cols = append(ns.cols, cols...)
+	return ns
+}
+
 func (s *stmt) SQL() string {
-	selectSql := s.selectStmt.sql()
-	return selectSql
+	ns := s.table.aliasOrName()
+	switch s.op {
+	case opSelect:
+		return fmt.Sprintf(
+			"%s %s FROM %q AS %q",
+			s.op,
+			s.cols.string(ns),
+			s.table.name,
+			ns,
+		)
+	case opDelete:
+		return fmt.Sprintf(
+			"%s FROM %q AS %q",
+			s.op,
+			s.table.name,
+			ns,
+		)
+	default:
+		return ""
+	}
 }
 
 func (s *stmt) clone() *stmt {
-	selectStmt := *s.selectStmt
-	ns := &stmt{
-		DB:         s.DB,
-		table:      s.table,
-		selectStmt: &selectStmt,
-	}
-	selectStmt.stmt = ns
-	return ns
+	ns := *s
+	return &ns
 }
--- stmt_test.go
+++ stmt_test.go
@@ -1,3 +1,27 @@
 package sua
 
-var mockStmt = newStmt(nil)
+import (
+	"testing"
+
+	"github.com/dundunlabs/sua/test"
+)
+
+var (
+	mockSelectStmt = &stmt{op: opSelect}
+	mockDeleteStmt = &stmt{op: opDelete}
+)
+
+func TestSelect(t *testing.T) {
+	s := mockSelectStmt.Table("users", "u")
+	s1 := s.Select("id", "name")
+	s2 := s1.Select("gender")
+
+	test.Equal(t, s.SQL(), `SELECT "u".* FROM "users" AS "u"`)
+	test.Equal(t, s1.SQL(), `SELECT "u"."id", "u"."name" FROM "users" AS "u"`)
+	test.Equal(t, s2.SQL(), `SELECT "u"."id", "u"."name", "u"."gender" FROM "users" AS "u"`)
+}
+
+func TestDelete(t *testing.T) {
+	s := mockDeleteStmt.Table("users", "u")
+	test.Equal(t, s.SQL(), `DELETE FROM "users" AS "u"`)
+}
